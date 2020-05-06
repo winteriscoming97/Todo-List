88a403a7f6466c347669ad3252a83b3ceaae7eb7
Justin <justinjackson1198@gmail.com>
2020-05-06 14:00:41 -0700

    added basic list layout, toggle scripts, and stylings

--- application.js
+++ application.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  
+  $('.check-box').click(toggleCheck);
+  $('.task').hover(toggleCancel);
 });
+
+var toggleCheck = function () {
+  $(this).children().toggle('fast');
+};
+
+var toggleCancel = function () {
+  $(this).children('.cancel').toggle();
+}
